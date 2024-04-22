746eb530c016886c2e64737a3e4de2372bd9b0df
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2024-04-22 14:05:54 +0200

    Creamos rama y holaMundo.

---
+++ PruebaConRamas/src/main/java/com/salesianostriana/dam/pruebaconramas/PruebaConRamasApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.pruebaconramas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaConRamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaConRamasApplication.class, args);
+	}
+
+}
---
+++ PruebaConRamas/src/test/java/com/salesianostriana/dam/pruebaconramas/PruebaConRamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.pruebaconramas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaConRamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
